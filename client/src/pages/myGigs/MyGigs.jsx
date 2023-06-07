983323d3c9e208c548545344e4a6ae1214346c0f
ngdkhuong <nguyenkhuong31072002@gmail.com>
2023-06-07 11:00:38 +0700

    upload

--- client/src/pages/myGigs/MyGigs.jsx
+++ client/src/pages/myGigs/MyGigs.jsx
@@ -1,7 +1,73 @@
 import './MyGigs.scss';
+import { Link } from 'react-router-dom';
 
 const MyGigs = () => {
-    return <div>MyGigs</div>;
+    return (
+        <div className="my-gigs">
+            <div className="container">
+                <div className="title">
+                    <h1>Gigs</h1>
+                    <Link to="/add">
+                        <button>Add New Gigs</button>
+                    </Link>
+                </div>
+                <table>
+                    <tr>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Sales</th>
+                        <th>Action</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img
+                                className="img"
+                                src="https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/148634416/original/8ba246bb778f365a85dc0267170ae0af4f6e61ef.jpg"
+                                alt=""
+                            />
+                        </td>
+                        <td>Gig1</td>
+                        <td>88</td>
+                        <td>123</td>
+                        <td>
+                            <img className="delete" src="/img/delete.png" alt="" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img
+                                className="img"
+                                src="https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/148634416/original/8ba246bb778f365a85dc0267170ae0af4f6e61ef.jpg"
+                                alt=""
+                            />
+                        </td>
+                        <td>Gig1</td>
+                        <td>88</td>
+                        <td>123</td>
+                        <td>
+                            <img className="delete" src="/img/delete.png" alt="" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img
+                                className="img"
+                                src="https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/148634416/original/8ba246bb778f365a85dc0267170ae0af4f6e61ef.jpg"
+                                alt=""
+                            />
+                        </td>
+                        <td>Gig1</td>
+                        <td>88</td>
+                        <td>123</td>
+                        <td>
+                            <img className="delete" src="/img/delete.png" alt="" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default MyGigs;
--- client/src/pages/orders/Orders.jsx
+++ client/src/pages/orders/Orders.jsx
@@ -1,7 +1,76 @@
-import React from 'react';
+import './Orders.scss';
+import { Link } from 'react-router-dom';
 
 const Orders = () => {
-    return <div>Orders</div>;
+    const currentUser = {
+        id: 1,
+        username: 'John Doe',
+        isSeller: true,
+    };
+
+    return (
+        <div className="orders">
+            <div className="container">
+                <div className="title">
+                    <h1>Orders</h1>
+                </div>
+                <table>
+                    <tr>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>{currentUser?.isSeller ? 'Buyer' : 'Seller'}</th>
+                        <th>Contact</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img
+                                className="img"
+                                src="https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/148634416/original/8ba246bb778f365a85dc0267170ae0af4f6e61ef.jpg"
+                                alt=""
+                            />
+                        </td>
+                        <td>Gig1</td>
+                        <td>88</td>
+                        <td>123</td>
+                        <td>
+                            <img className="message" src="/img/message.png" alt="" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img
+                                className="img"
+                                src="https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/148634416/original/8ba246bb778f365a85dc0267170ae0af4f6e61ef.jpg"
+                                alt=""
+                            />
+                        </td>
+                        <td>Gig1</td>
+                        <td>88</td>
+                        <td>123</td>
+                        <td>
+                            <img className="message" src="/img/message.png" alt="" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img
+                                className="img"
+                                src="https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/148634416/original/8ba246bb778f365a85dc0267170ae0af4f6e61ef.jpg"
+                                alt=""
+                            />
+                        </td>
+                        <td>Gig1</td>
+                        <td>88</td>
+                        <td>123</td>
+                        <td>
+                            <img className="message" src="/img/message.png" alt="" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default Orders;
