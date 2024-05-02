b37a982e8c80a834d2e69bd2a5d0a895859d28f3
Dang Khuong Nguyen <91656145+ngdkhuong@users.noreply.github.com>
2024-05-02 23:03:27 +0700

    change ctrl

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,47 +1,58 @@
-import User from '../models/user.model.js';
-import createError from '../utils/createError.js';
-import bcryptjs from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
-    try {
-        const hash = bcryptjs.hashSync(req.body.password, 5);
-        const newUser = new User({
-            ...req.body,
-            password: hash,
-        });
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    });
 
-        await newUser.save();
-        res.status(201).send('User has been created.');
-    } catch (error) {
-        next(error);
-    }
+    await newUser.save();
+    res.status(201).send("User has been created.");
+  } catch (err) {
+    next(err);
+  }
 };
-
 export const login = async (req, res, next) => {
-    try {
-        const user = await User.findOne({ username: req.body.username });
-        if (!user) return next(createError(404, 'User not found!'));
+  try {
+    const user = await User.findOne({ username: req.body.username });
 
-        const isCorrect = bcryptjs.compareSync(req.body.password, user.password);
-        if (!isCorrect) return next(createError(400, 'Wrong password'));
+    if (!user) return next(createError(404, "User not found!"));
 
-        const token = jwt.sign(
-            {
-                id: user._id,
-                isSeller: user.isSeller,
-            },
-            process.env.JWT_KEY,
-        );
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect)
+      return next(createError(400, "Wrong password or username!"));
 
-        const { password, ...info } = user._doc;
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
 
-        res.cookie('accessToken', token, { httpOnly: true }).status(200).send(info);
-    } catch (error) {
-        next(error);
-    }
+    const { password, ...info } = user._doc;
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const logout = async (req, res, next) => {
-    res.clearCookie('accessCookie', { sameSite: 'none', secure: true }).status(200).send('User has been logged out');
+export const logout = async (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("User has been logged out.");
 };
